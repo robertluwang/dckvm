57ea46ba0a8e11cf8b04075925fa4c53b51988f2
Robert Wang <robert.lu.wang@gmail.com>
2018-12-22 15:44:36 -0500

    Update kvm_image_build.sh

--- kvm_image_build.sh
+++ kvm_image_build.sh
@@ -13,6 +13,6 @@ virt-builder $OS_VERSION \
 --size $DISKSIZE -o $IMGPATH/${OS_VERSION}-$DISKSIZE.qcow2 \
 --network --timezone $TZ \
 --root-password password:$ROOTPW \
---firstboot-command "sed -i 's/^SELINUX=.*/SELINUX=permissive/g' /etc/selinux/config; systemctl stop NetworkManager; systemctl disable NetworkManager; systemctl restart network"
+--firstboot-command "sed -i 's/^SELINUX=.*/SELINUX=permissive/g' /etc/selinux/config; systemctl stop NetworkManager; systemctl disable NetworkManager"
 
 virt-sysprep -a $IMGPATH/${OS_VERSION}-$DISKSIZE.qcow2
