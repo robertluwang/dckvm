5de7e3277bf21df089d687bb13d0a5012a60a436
Robert Wang <robert.lu.wang@gmail.com>
2018-12-21 19:44:41 -0500

    Update kvm_image_build.sh

--- kvm_image_build.sh
+++ kvm_image_build.sh
@@ -9,8 +9,10 @@ dckvm=`dirname "$0"`
 source $dckvm/kvmrc
 
 virt-builder $OS_VERSION \
+--format qcow2 \
 --size $DISKSIZE -o $IMGPATH/${OS_VERSION}-$DISKSIZE.img \
 --network --timezone $TZ \
 --root-password password:$ROOTPW \
 --firstboot-command "sed -i 's/^SELINUX=.*/SELINUX=permissive/g' /etc/selinux/config; systemctl stop NetworkManager; systemctl disable NetworkManager; systemctl restart network"
 
+virt-sysprep -a $IMGPATH/${OS_VERSION}-$DISKSIZE.img
